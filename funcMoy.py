57b79c1dc467c8a5d0b1b8184208f3064bc6cfa7
sim001 <simon.amoyal4@gmail.com>
2019-11-06 16:54:57 +0100

    second

---
+++ funcMoy.py
@@ -0,0 +1,10 @@
+import sys
+
+
+x = int( sys.argv[1] )
+y = int( sys.argv[2] )
+moy = (x + y)/2
+
+print(sys.argv)
+
+print ('moyenne : ',moy)
---
+++ funcPairImpair.py
@@ -0,0 +1,13 @@
+import datetime
+
+
+date = datetime.datetime.now()
+
+
+day = date.day
+
+print(day)
+if 'day%2 = 0':
+    print("pair")
+else:    
+    print("impair")
